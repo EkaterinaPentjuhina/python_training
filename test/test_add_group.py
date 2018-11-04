e73dffc17be9b3e77469aa9ba6d9fe31cb414ae3
EkaterinaPentjuhina <katkarach@gmail.com>
2018-11-04 23:28:47 +0300

    Ожидание выхода на стартовую страницу в методе Logout

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,13 +2,13 @@
 from model.group import Group
 
 
-def test_add_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="asdf", header="zxcv", footer="qwee"))
-    app.session.logout()
-
-
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
+    app.session.logout()
+
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="asdf", header="zxcv", footer="qwee"))
     app.session.logout()
