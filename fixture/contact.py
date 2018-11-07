50bc8c1e237688f743531190bb3902f5343ff515
EkaterinaPentjuhina <katkarach@gmail.com>
2018-11-07 18:41:47 +0300

    Правка метода удаления первого контакта (wd.switch_to.alert.accept())

--- fixture/contact.py
+++ fixture/contact.py
@@ -106,7 +106,7 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
-        wd.switch_to_alert().accept()
+        wd.switch_to.alert.accept()
 
     def edit_first_contact(self, contact):
         wd = self.app.wd
