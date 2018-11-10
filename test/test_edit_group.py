35d4096bd2f8055dd8ed3a2b018490a06adb179b
EkaterinaPentjuhina <katkarach@gmail.com>
2018-11-10 15:48:02 +0300

    Оптимизация ввода данных в тестах модификации группы

--- test/test_edit_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="group1", header="group1_header", footer="group1_footer"))
-    app.session.logout()
