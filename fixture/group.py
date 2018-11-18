92eb6f4dd6315ad0c3c9573ac9c89f015c7331fc
EkaterinaPentjuhina <katkarach@gmail.com>
2018-11-18 19:49:04 +0300

    Загрузка и сравнение списка групп в тестах

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,5 @@
+from model.group import Group
+
 
 class GroupHelper:
 
@@ -70,5 +72,16 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
+
 
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,17 +4,26 @@ from model.group import Group
 def test_modify_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test group"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="group1", header="group1_header", footer="group1_footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test group"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test group"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
