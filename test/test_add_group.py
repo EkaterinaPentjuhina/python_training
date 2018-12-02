2a6fd0fe34b55647d5ff7a5abbbc48e8f3803c48
EkaterinaPentjuhina <katkarach@gmail.com>
2018-12-02 19:59:43 +0300

    Генератор тестов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-import random
-import string
-from data.add_group import constant as testdata
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     new_groups = app.group.get_group_list()
