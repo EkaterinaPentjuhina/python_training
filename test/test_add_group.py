267333afc873d9ce7978c4749e2442a89efb8239
EkaterinaPentjuhina <katkarach@gmail.com>
2018-12-02 19:43:17 +0300

    Генерация тестовых данных в формате json (groups)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,25 +3,7 @@ from model.group import Group
 import pytest
 import random
 import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(2)
-]
-
-#
-# testdata = [
-#     Group(name=name, header=header, footer=footer)
-#     for name in ["", random_string("name", 10)]
-#     for header in ["", random_string("header", 20)]
-#     for footer in ["", random_string("footer", 10)]
-# ]
+from data.add_group import constant as testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
@@ -34,12 +16,4 @@ def test_add_group(app, group):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
