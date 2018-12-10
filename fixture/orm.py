344fb2ab8d5832ead1ba17caebd0e5956787eb5b
EkaterinaPentjuhina <katkarach@gmail.com>
2018-12-11 01:05:56 +0300

    Получение списка групп с помощью ORM

---
+++ fixture/orm.py
@@ -0,0 +1,29 @@
+from pony.orm import *
+from datetime import datetime
+
+
+class ORMFixture:
+
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = 'group_list'
+        id = PrimaryKey(int, column='group_id')
+        name = Optional(str, column='group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        id = PrimaryKey(int, column='id')
+        firstname = Optional(str, column='firstname')
+        lastname = Optional(str, column='lastname')
+        deprecated = Optional(datetime, column='deprecated')
+
+    def __init__(self, host, name, user, password):
+        self.db.bind('mysql', host=host, database=name, user=user, password=password)
+        self. db.generate_mapping()
+
+    @db_session
+    def get_group_list(self):
+        return list(select(g for g in ORMFixture.ORMGroup))
