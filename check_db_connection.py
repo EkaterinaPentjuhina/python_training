0640b3234f7a9a46b21e8ff00bc4dbfc68adfec0
EkaterinaPentjuhina <katkarach@gmail.com>
2018-12-11 01:30:04 +0300

    Получение списка контактов с помощью ORM

--- check_db_connection.py
+++ check_db_connection.py
@@ -3,7 +3,7 @@ from fixture.orm import ORMFixture
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_group_list()
+    l = db.get_contact_list()
     for item in l:
         print(item)
     print(len(l))
--- fixture/orm.py
+++ fixture/orm.py
@@ -1,5 +1,7 @@
 from pony.orm import *
 from datetime import datetime
+from model.group import Group
+from model.contact_properties import Contact
 
 
 class ORMFixture:
@@ -23,7 +25,22 @@ class ORMFixture:
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password)
         self. db.generate_mapping()
+        sql_debug(True)
+
+    def convert_groups_to_model(self, groups):
+        def convert(group):
+            return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
+        return list(map(convert, groups))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
+        return list(map(convert, contacts))
 
     @db_session
     def get_group_list(self):
-        return list(select(g for g in ORMFixture.ORMGroup))
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
